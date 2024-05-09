ef89c80d2deff2a1bab5bc71b61afdc788161893
Matt <97248295+MattOrbtl@users.noreply.github.com>
2024-05-08 20:45:59 -0400

    update users schema

--- apps/server/src/database/schemas/User.ts
+++ apps/server/src/database/schemas/User.ts
@@ -1,17 +1,14 @@
 import mongoose from 'mongoose';
+import { User } from '../../utils/interfaces';
 
-interface IUser {
-  username: string;
-  email: string;
-  password: string;
-  accountId: mongoose.Schema.Types.ObjectId;
-}
-
-const userSchema = new mongoose.Schema<IUser>({
+const userSchema = new mongoose.Schema<User>({
   username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  accountId: { type: mongoose.Schema.Types.ObjectId, ref: 'Account' }
+  email: { type: String, required: true, unique: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  accountId: { type: mongoose.Schema.Types.ObjectId, ref: 'Account' },
+  passwordLastUpdatedAt: { type: Date, default: Date.now }
 });
 
 export default userSchema;
